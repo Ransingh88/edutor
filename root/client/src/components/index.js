4ef9e927a010e22ef5e357a1556979bc825d1b87
Ransingh88 <rajaransingh@gmail.com>
2021-10-09 04:59:39 +0530

    imported new components

--- root/client/src/components/index.js
+++ root/client/src/components/index.js
@@ -1,3 +1,5 @@
 export {default as ChannelListContainer} from './ChannelListContainer/ChannelListContainer'
 export {default as ChannelContainer} from './channelContainer/ChannelContainer'
 export {default as ChatApp} from './chatApp/ChatApp'
+export {default as ChannelSearch} from './channelSearch/ChannelSearch'
+export {default as TeamChannelList} from './teamChannelList/TeamChannelList'
