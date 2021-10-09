6d26962044620aa34efe455250e3bf2993a78ade
Ransingh88 <rajaransingh@gmail.com>
2021-10-09 16:29:35 +0530

    caht app props passed and state created

--- root/client/src/components/chatApp/ChatApp.jsx
+++ root/client/src/components/chatApp/ChatApp.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 import './ChatApp.css'
+import 'stream-chat-react/dist/css/index.css'
 import {ChannelContainer, ChannelListContainer, Auth} from '../index'
 
 const cookies = new Cookies()
@@ -24,12 +25,28 @@ if (authToken) {
 }
 
 const ChatApp = () => {
+
+    const [createType, setCreateType] = useState('')
+    const [isCreating, setIsCreating] = useState(false)
+    const [isEditing, setIsEditing] = useState(false)
+
     if(!authToken) return <Auth/>
     return (
         <div className="chat">
             <Chat client={client} theme="team light">
-                <ChannelListContainer />
-                <ChannelContainer/>
+                <ChannelListContainer
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
+                />
+                <ChannelContainer
+                isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}
+                />
             </Chat>
         </div>
     )
