bd4503fcc31d374337231edacf6838169be3950a
Ransingh88 <rajaransingh@gmail.com>
2021-10-09 14:54:37 +0530

    server index

---
+++ root/server/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const authRoutes = require('./routes/auth')
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.use('/auth', authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}...`)
+})
