4f93fc74c2a7707bb34e480b7e98058d6384436c
Ransingh88 <rajaransingh@gmail.com>
2021-10-09 05:47:10 +0530

    auth created

---
+++ root/client/src/components/auth/Auth.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Cookies from 'universal-cookie'
+import axios from 'axios'
+
+const Auth = () => {
+    const [isSignup, setIsSignUp] = useState(false)
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Auth
