7db9176983cb6f3a0b7edda1834bd795d6ed02ad
Ransingh88 <rajaransingh@gmail.com>
2021-10-09 16:29:07 +0530

    channel containe r code updated

--- root/client/src/components/channelContainer/ChannelContainer.jsx
+++ root/client/src/components/channelContainer/ChannelContainer.jsx
@@ -2,10 +2,48 @@ import React from 'react'
 import {Channel, useChatContext} from 'stream-chat-react'
 import {ChannelInner, CreateChannel, EditChannel, TeamMessage} from '../index'
 
-const ChannelContainer = () => {
+const ChannelContainer = ({
+    isCreating,
+setIsCreating,
+isEditing,
+setIsEditing,
+createType}) => {
+    const { channel } = useChatContext();
+
+    if (isCreating) {
+        return (
+            <div className="channelContainer">
+                <CreateChannel createType={createType} setIsCreating={ setIsCreating}/>
+            </div>
+        )
+    }
+
+    if (isEditing) {
+        return (
+            <div className="channelContainer">
+                <EditChannel setIsEditing={ setIsEditing}/>
+            </div>
+        )
+    }
+
+    const EmptyState = () => {
+        
+            <div className="channelEmpty__container">
+                <p className="channelEmpty__first">This is your biggining of your chat history</p>
+                <p className="channelEmpty__second">Send messages, attachments, links, emojis and more!</p>
+            </div>
+        
+    }
+
+
     return (
-        <div>
-            ChannelContainer
+        <div className="channelContainer">
+            <Channel
+                EmptyStateIndicator={EmptyState}
+                Message={(messageProps, i) => <TeamMessage key={i}{...messageProps}/>}
+            >
+                <ChannelInner setIsEditing={ setIsEditing}/>
+            </Channel>
         </div>
     )
 }
