ffca3901d5927d9cafaf1ec5b6df7ab150432fb1
Ransingh88 <rajaransingh@gmail.com>
2021-10-10 14:49:07 +0530

    Auth form css added

--- root/client/src/components/auth/Auth.jsx
+++ root/client/src/components/auth/Auth.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Cookies from 'universal-cookie'
 import axios from 'axios'
+import './AuthForm.css'
+import {Link} from 'react-router-dom'
 
 const cookies = new Cookies()
 
@@ -43,47 +45,50 @@ const Auth = () => {
         setIsSignUp((prevIsSignup) => !prevIsSignup)
     }
 
-     return (
-        <div>
-            <p>{isSignup ? "Sign Up" : "Sign In"}</p>
-            <form onSubmit={handleSubmit}>
+    return (
+        <>
+            
+            <div className="AuthForm">
+                <Link to="/"><p>BacktoHome</p></Link>
+            <p className="AuthForm__Heading">{isSignup ? "Fill Details" : "Enter Credential to chat"}</p>
+            <form onSubmit={handleSubmit} className="AuthForm__form">
                 {isSignup && (
                     <div>
-                        <label htmlFor="fullName">Name</label>
+                        <label htmlFor="fullName">Name</label> <br/>
                         <input type="text" name="fullName" placeholder="Full Name" onChange={handleChange} required/>
                     </div>
                 )}
                 <div>
-                        <label htmlFor="username">UserName</label>
+                        <label htmlFor="username">UserName</label><br/>
                         <input type="text" name="username" placeholder="User Name" onChange={handleChange} required/>
                 </div>
                 {isSignup && (
                     <div>
-                        <label htmlFor="phone">Phone</label>
+                        <label htmlFor="phone">Phone</label><br/>
                         <input type="text" name="phone" placeholder="Phone no" onChange={handleChange} required/>
                     </div>
                 )}
                 {isSignup && (
                     <div>
-                        <label htmlFor="AvatarURL">Avatar URL</label>
+                        <label htmlFor="AvatarURL">Avatar URL</label><br/>
                         <input type="text" name="AvatarURL" placeholder="Avatar URL" onChange={handleChange} required/>
                     </div>
                 )}
                 <div>
-                        <label htmlFor="password">Password</label>
+                        <label htmlFor="password">Password</label><br/>
                         <input type="password" name="password" placeholder="Password" onChange={handleChange} required/>
                 </div>
                 {isSignup && (
                     <div>
-                        <label htmlFor="confirmPassword">confirm Password</label>
-                        <input type="text" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} required/>
+                        <label htmlFor="confirmPassword">confirm Password</label><br/>
+                        <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} required/>
                     </div>
                  )}
                  <div>
                      <button>{isSignup? "Sign up" : "Sign in" }</button>
                  </div>
             </form>
-            <div>
+            <div className="AuthForm__link">
                 <p>
                     {isSignup ? "Already have an account?" : "Don't have an account?"}
                     <span onClick={switchMode}>
@@ -92,6 +97,7 @@ const Auth = () => {
                 </p>
             </div>
         </div>
+        </>
     )
 }
 
